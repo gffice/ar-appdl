30c78cdda79b89e5adb267ec7dc3f4c687bd6e5d
Game Office <114347535+gffice@users.noreply.github.com>
2026-03-01 17:09:49 +0800

    Update installarti.sh

--- installarti.sh
+++ installarti.sh
@@ -8,13 +8,13 @@ wget https://gffice.github.io/ar-appdl/bin-amd64.tar.xz
 tar -xJvf bin-amd64.tar.xz
 cd bin
 md5sum -c md5
-cp lyrebird snowflake webtunnel conjure arti /opt/arti/
+sudo cp lyrebird snowflake webtunnel conjure arti /opt/arti/
 cd ../..
-chmod +x -R /opt/arti
-cp arti.toml /opt/arti
-chown -R arti /opt/arti
+sudo chmod +x -R /opt/arti
+sudo cp arti.toml /opt/arti
+sudo chown -R arti /opt/arti
 
-cp arti.service /etc/systemd/system/arti.service
+sudo cp arti.service /etc/systemd/system/arti.service
 sudo systemctl daemon-reload
 sudo systemctl enable arti.service
 rm -r tmp
