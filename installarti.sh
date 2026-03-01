07db9c96d923bd0e1994541c0435935e1ad2af94
Game Office <114347535+gffice@users.noreply.github.com>
2026-03-01 16:07:59 +0800

    Add files via upload

---
+++ installarti.sh
@@ -0,0 +1,18 @@
+mkdir tmp
+cd tmp
+
+sudo useradd -r -s /usr/sbin/nologin -M arti
+sudo mkdir /opt/arti
+
+wget https://gffice.github.io/ar-appdl/bin-amd64.tar.xz
+tar -xJvf bin-amd64.tar.xz
+cd bin
+md5sum -c md5
+cp lyrebird snowflake webtunnel conjure arti /opt/arti/
+chmod +x -R /opt/arti
+cp ../../arti.toml /opt/arti
+chown -R arti /opt/arti
+
+cp ../../arti.service /etc/systemd/system/arti.service
+sudo systemctl daemon-reload
+sudo systemctl enable arti.service
